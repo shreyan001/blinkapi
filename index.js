d1df06dd477783af9e9abfb8d8c6a509eda654e2
shreyan001 <shreyan010@gmail.com>
2023-02-10 13:22:07 +0530

    push

--- index.js
+++ index.js
@@ -1,13 +1,156 @@
 const express = require('express');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const User = require('./models/User');
+const Table = require('./models/Tables')
+
+
+require('dotenv').config();
+
+
 const app = express();
-const path = require('path');
 
-app.use(express.static('public'))
+app.use(morgan('dev'));
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
 
+mongoose.set('strictQuery', true);
+mongoose.connect(process.env.DB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.use(express.static('public'));
 app.get('/', (req, res) => {
-    res.sendFile('index.html', {root: path.join(__dirname, 'public')});
-})
+  res.json({
+    message: '🦄🌈✨👋🌎🌍🌏✨🌈🦄'
+  });
+});
 
-app.listen(process.env.PORT || 3000);
+app.post('/api/register', async (req,res) => {
+  const {img,name,addr} = req.body;
+  try{
+    const userDoc = await User.create({
+      image:img,
+      name:name,
+      address:addr
+    });
+    res.json(userDoc);
+  } catch(e) {
+    console.log(e);
+    res.status(400).json(e);
+  }
+});
 
-module.exports = app;
+app.post('/api/login', async (req,res) => {
+  const {address} = req.body;
+  const userDoc = await User.findOne({address:address});
+  if(userDoc == null){
+    res.status(404).json("error")}
+    else {
+      res.json({userDoc});
+    }
+
+   
+});
+
+
+app.get('/api/tables', async (req,res) => {
+  
+    const userDoc = await Table.find();
+    if(userDoc == null){
+      res.status(404).json("error")}
+      else {
+        res.json({userDoc});
+      }
+  
+     
+  });
+
+
+  app.put('/api/tables/:id', async (req,res) => {
+    const {addr} = req.body;
+    const name = req.params['id'];
+    const userDoc = await Table.findOneAndUpdate({name},
+      { $addToSet : {addresses: addr}  } );
+    if(userDoc == null){
+      res.status(404).json("error")}
+      else {
+        res.json({userDoc});
+      }
+   });
+
+   app.delete('/api/tables/:id', async (req,res) => {
+    const {addr} = req.body;
+    const name = req.params['id'];
+    const userDoc = await Table.findOneAndUpdate({tableName:name},
+      { $pull : {addresses: addr}  } );
+    if(userDoc == null){
+      res.status(404).json("error")}
+      else {
+        res.json({userDoc});
+      }
+   });
+
+
+
+
+
+   app.get('/api/tables/:id', async (req,res) => {
+    
+    const name = req.params['id'];
+    const userDoc = await Table.findOne({tableName:name}
+     ) ;
+    if(userDoc == null){
+      res.status(404).json("error")}
+      else {
+        res.json(userDoc.addresses);
+      }
+     
+   });
+
+   app.post('/api/Users/1', async (req,res) => {
+   
+    const {addr} = req.body;
+    const userDoc = await User.findOne ({addr});
+    if(userDoc == null){
+      res.status(404).json("error")}
+      else {
+      
+        res.json(userDoc);
+      }
+  
+     
+  });  
+
+
+
+  app.post('/api/users', (req, res) => {
+    const addresses = req.body.addr;
+  
+    if (!addresses || !addresses.length) {
+      return res.send([]);
+    }
+  
+    User.find({
+      $or: addresses.map(address => ({ address }))
+    }, (err, users) => {
+      if (err) {
+        return res.send(err);
+      }
+  
+      res.send(users);
+    });
+  });
+  const port = process.env.PORT || 3200;
+  app.listen(port, () => {
+   
+    /* eslint-disable no-console */
+    console.log(`Listening: http://localhost:${port}`);
+    /* eslint-enable no-console */
+  });
+
+  module.exports = app;
